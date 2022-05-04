f891f318dec06f3a661ab6349a9e384ce3d45a62
Jarkendar <jarek.skrzypczak@o2.pl>
2022-05-04 23:17:31 +0200

    Add viewModel guts

--- app/src/main/java/com/skrzypczak/charactergenerator/CharacterViewModel.kt
+++ app/src/main/java/com/skrzypczak/charactergenerator/CharacterViewModel.kt
@@ -1,8 +1,85 @@
 package com.skrzypczak.charactergenerator
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CharacterViewModel: ViewModel() {
 
+    private val _characterName = MutableLiveData<String>().apply { postValue("") }
+    val characterName: LiveData<String> = _characterName
 
+    private val _race = MutableLiveData<String>().apply { postValue("") }
+    val race: LiveData<String> = _race
+
+    private val _attrStrong = MutableLiveData<Int>().apply { postValue(3) }
+    val attrStrong: LiveData<Int> = _attrStrong
+
+    private val _attrWisdom = MutableLiveData<Int>().apply { postValue(3) }
+    val attrWisdom: LiveData<Int> = _attrWisdom
+
+    private val _attrAgility = MutableLiveData<Int>().apply { postValue(3) }
+    val attrAgility: LiveData<Int> = _attrAgility
+
+    private val _attrSpirit = MutableLiveData<Int>().apply { postValue(3) }
+    val attrSpirit: LiveData<Int> = _attrSpirit
+
+    private val _attrWit = MutableLiveData<Int>().apply { postValue(2) }
+    val attrWit: LiveData<Int> = _attrWit
+
+    private val _inspirationLimit = MutableLiveData<Int>().apply { postValue(4) }
+    val inspirationLimit: LiveData<Int> = _inspirationLimit
+
+    private val _damageLimit = MutableLiveData<Int>().apply { postValue(5) }
+    val damageLimit: LiveData<Int> = _damageLimit
+
+    private val _fearLimit = MutableLiveData<Int>().apply { postValue(5) }
+    val fearLimit: LiveData<Int> = _fearLimit
+
+    private val _passiveSkill = MutableLiveData<String>().apply { postValue("") }
+    val passiveSkill: LiveData<String> = _passiveSkill
+
+    fun setCharacterName(name: String) {
+        _characterName.value = name
+    }
+
+    fun setRace(race: String) {
+        _race.value = race
+    }
+
+    fun setStrong(value: Int) {
+        _attrStrong.value = value
+    }
+
+    fun setWisdom(value: Int) {
+        _attrWisdom.value = value
+    }
+
+    fun setAgility(value: Int) {
+        _attrAgility.value = value
+    }
+
+    fun setSpirit(value: Int) {
+        _attrSpirit.value = value
+    }
+
+    fun setWit(value: Int) {
+        _attrWit.value = value
+    }
+
+    fun setInspirationLimit(value: Int) {
+        _inspirationLimit.value = value
+    }
+
+    fun setDamageLimit(value: Int) {
+        _damageLimit.value = value
+    }
+
+    fun setFearLimit(value: Int) {
+        _fearLimit.value = value
+    }
+
+    fun setPassiveSkill(text: String) {
+        _passiveSkill.value = text
+    }
 }
--- app/src/main/java/com/skrzypczak/charactergenerator/MainActivity.kt
+++ app/src/main/java/com/skrzypczak/charactergenerator/MainActivity.kt
@@ -2,14 +2,77 @@ package com.skrzypczak.charactergenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.Spinner
 import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.skrzypczak.charactergenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val model: CharacterViewModel by viewModels()
+    private val model: CharacterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = model
+
+        findViewById<EditText>(R.id.edit_character_name).addTextChangedListener {
+            model.setCharacterName(it.toString())
+        }
+
+        findViewById<Spinner>(R.id.spinner_race).onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                model.setRace(parent?.getItemAtPosition(position).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //ignore
+            }
+        }
+
+        findViewById<SeekBar>(R.id.seekBar_strong).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setStrong(it) })
+        findViewById<SeekBar>(R.id.seekBar_wisdom).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setWisdom(it) })
+        findViewById<SeekBar>(R.id.seekBar_agility).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setAgility(it) })
+        findViewById<SeekBar>(R.id.seekBar_spirit).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setSpirit(it) })
+        findViewById<SeekBar>(R.id.seekBar_wit).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setWit(it) })
+        findViewById<SeekBar>(R.id.seekBar_inspiration).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setInspirationLimit(it) })
+        findViewById<SeekBar>(R.id.seekBar_damage).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setDamageLimit(it) })
+        findViewById<SeekBar>(R.id.seekBar_fear).setOnSeekBarChangeListener(createSeekBarChangeListener { model.setFearLimit(it) })
+
+        findViewById<EditText>(R.id.edit_passive_skill).addTextChangedListener {
+            model.setPassiveSkill(it.toString())
+        }
     }
+
+    private fun createSeekBarChangeListener(setAttr: (value: Int) -> Unit) = object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                setAttr(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //ignore
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                //ignore
+            }
+        }
+
+
 }
