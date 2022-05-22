81ffb90d49d91e805d155386df131e5cbb37d396
Jarkendar <jarek.skrzypczak@o2.pl>
2022-05-22 09:00:11 +0200

    Add save bitmap and send to email

--- app/src/main/java/com/skrzypczak/charactergenerator/MainActivity.kt
+++ app/src/main/java/com/skrzypczak/charactergenerator/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.skrzypczak.charactergenerator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.View
-import android.widget.AdapterView
-import android.widget.EditText
-import android.widget.SeekBar
-import android.widget.Spinner
+import android.widget.*
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import androidx.core.widget.NestedScrollView
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
 import com.skrzypczak.charactergenerator.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     private val model: CharacterViewModel by viewModels()
+
+    private val mainScope = CoroutineScope(Dispatchers.Main + Job())
+    private val ioScope = CoroutineScope(Dispatchers.IO + Job())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,6 +69,67 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<EditText>(R.id.edit_passive_skill).addTextChangedListener {
             model.setPassiveSkill(it.toString())
+        }
+
+        findViewById<Button>(R.id.button_done).setOnClickListener { _ ->
+            val layout = findViewById<NestedScrollView>(R.id.main_layout)
+            val bitmap = Bitmap.createBitmap(layout.width, layout.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            layout.draw(canvas)
+            ioScope.launch {
+                try {
+                    val uri = saveBitmap(this@MainActivity.applicationContext, bitmap, Bitmap.CompressFormat.PNG, "image/png", "test.png")
+
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        putExtra(Intent.EXTRA_EMAIL, "arageros96@gmail.com")
+                        putExtra(Intent.EXTRA_SUBJECT, "On The Job")
+                        putExtra(Intent.EXTRA_STREAM, uri)
+                        type = "image/png"
+                    }
+                    mainScope.launch {
+                        startActivity(Intent.createChooser(intent, "Share you on the jobing"))
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    fun saveBitmap(
+        context: Context, bitmap: Bitmap, format: Bitmap.CompressFormat,
+        mimeType: String, displayName: String
+    ): Uri {
+
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
+            put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+        }
+
+        val resolver = context.contentResolver
+        var uri: Uri? = null
+
+        try {
+            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                ?: throw IOException("Failed to create new MediaStore record.")
+
+            resolver.openOutputStream(uri)?.use {
+                if (!bitmap.compress(format, 95, it))
+                    throw IOException("Failed to save bitmap.")
+            } ?: throw IOException("Failed to open output stream.")
+
+            return uri
+
+        } catch (e: IOException) {
+
+            uri?.let { orphanUri ->
+                // Don't leave an orphan entry in the MediaStore
+                resolver.delete(orphanUri, null, null)
+            }
+
+            throw e
         }
     }
 
