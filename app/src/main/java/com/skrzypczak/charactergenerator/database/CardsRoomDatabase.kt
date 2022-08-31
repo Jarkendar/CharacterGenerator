334ad7b9ae1d689c74bc2b7b38b66e2202b18dfb
Jarkendar <jarek.skrzypczak@o2.pl>
2022-08-31 22:05:45 +0200

    Update version

--- app/src/main/java/com/skrzypczak/charactergenerator/database/CardsRoomDatabase.kt
+++ app/src/main/java/com/skrzypczak/charactergenerator/database/CardsRoomDatabase.kt
@@ -4,7 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [CardModel::class], version = 1, exportSchema = false)
+@Database(
+    entities = [CardModel::class],
+    version = 1,
+    exportSchema = true,
+)
 @TypeConverters(Converters::class)
 abstract class CardsRoomDatabase: RoomDatabase() {
 
